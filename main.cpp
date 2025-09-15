b18e586fe7d723fd8ed45d57ca53941e0ade0e3d
Hannah Saeed <hannahsaeed@Hannahs-MacBook-Pro.local>
2025-09-14 21:40:01 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,2 +1,12 @@
 #include <iostream>
 #include <fstream>
+using namespace std;
+
+string* reverseArray(string* array);
+void displayArray(string* array);
+
+int main()
+{
+    string* arr = new string[5]; 
+
+}
