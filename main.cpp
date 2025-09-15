5efd6c08d0886170b2dcde41d5f7de87df5f15dd
Hannah Saeed <hannahsaeed@Hannahs-MacBook-Pro.local>
2025-09-14 21:49:35 -0700

    Origin main

--- main.cpp
+++ main.cpp
@@ -7,6 +7,27 @@ void displayArray(string* array);
 
 int main()
 {
-    string* arr = new string[5]; 
+    string* arr = new string[5];
+
 
 }
+string* reverseArray(string* array)
+{
+    for(int i = 0; i <5; i++)
+    {
+        cout << "Enter Name #" << i << ":";
+        cin >> *(array + i);
+
+    }
+
+    for(int i = 0; i < 2; i++)
+    {
+        
+
+
+
+
+    }
+    return array;
+}
+
